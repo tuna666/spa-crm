889b2dcdc13a8b38e319abf9507caddabbae08e6
tuna <ev0.kpx@gmail.com>
2023-02-26 07:04:32 +0900

    マイグレーションファイル・モデル・コントローラーを作成

---
+++ backend/src/database/migrations/2023_02_26_065836_create_people_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('last_name');
+            $table->string('first_name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('mobile_number')->nullable();
+            $table->string('home_address')->nullable();
+            $table->string('home_zip')->nullable();
+            $table->text('memo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+};
