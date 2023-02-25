b37e8f33b4dd59229ae8d315430dc2f37c1b9064
tuna <ev0.kpx@gmail.com>
2023-02-26 07:30:24 +0900

    レスポンスに使うリソースクラスを作成

---
+++ backend/src/app/Http/Resources/PersonResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PersonResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'last_name' => $this->last_name,
+            'first_name' => $this->first_name,
+            'email' => $this->email,
+            'mobile_number' => $this->mobile_number,
+            'home_zip' => $this->home_zip,
+            'memo' => $this->memo,
+            'created_at' => $this->created_at->isoFormat('YYYY/M/D/(ddd) HH:mm:ss'),
+            'updated_at' => $this->updated_at->isoFormat('YYYY/M/D/(ddd) HH:mm:ss'),
+        ];
+    }
+}
