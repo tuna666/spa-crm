d6c3331a83146b7df374ca59beb949e6a3bdfeb7
tuna <ev0.kpx@gmail.com>
2023-03-01 08:11:05 +0900

    担当者一覧機能を実装

--- frontend/breeze-next/src/pages/person/index.jsx
+++ frontend/breeze-next/src/pages/person/index.jsx
@@ -11,17 +11,6 @@ import {
 } from '@mui/x-data-grid'
 import useSWR from "swr";
 
-// const rows = [
-//     {id: 1, name: 'JIN', kana: 'ジン', birth: '1992年12月4日'},
-//     {id: 2, name: 'SUGA', kana: 'シュガ', birth: '1993年3月9日'},
-//     {id: 3, name: 'J-HOPE', kana: 'ジェイホープ', birth: '1994年2月18日'},
-//     {id: 4, name: 'RM', kana: 'アールエム', birth: '1994年9月12日'},
-//     {id: 5, name: 'JIMIN', kana: 'ジミン', birth: '1995年10月13日'},
-//     {id: 6, name: 'V', kana: 'ヴィ', birth: '1995年12月30日'},
-//     {id: 7, name: 'JUNG KOOK', kana: 'ジョングク', birth: '1997年9月1日'}
-// ]
-
-
 function CustomToolbar() {
     return (
         <GridToolbarContainer>
@@ -34,11 +23,8 @@ function CustomToolbar() {
 export default function Index() {
 
     const fetcher = (url) => fetch(url).then((res) => res.json())
-    const {data, mutate} = useSWR(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/people`, fetcher)
+    const {data} = useSWR(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/people`, fetcher)
     const people = data?.data
-    console.log(people);
-    // const [people, setResponses] = useState([])
-
     let rows = [];
 
     people &&
@@ -56,22 +42,6 @@ export default function Index() {
             })
         }
     )
-
-
-    // const cols = [
-    //     {
-    //         field: 'name',
-    //         headerName: '英字'
-    //     },
-    //     {
-    //         field: 'kana',
-    //         headerName: '仮名'
-    //     },
-    //     {
-    //         field: 'birth',
-    //         headerName: '生年月日'
-    //     }
-    // ]
 
     const cols = [
         {
